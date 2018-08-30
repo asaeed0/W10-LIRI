a2039f46ffd4f91a43f8f6e57773be57a6136847
amaan <17208409+amaansaeed@users.noreply.github.com>
2018-08-30 10:08:22 -0400

    Fully functional - Needs to be debugged

--- liri.js
+++ liri.js
@@ -7,7 +7,14 @@ const fs = require("fs");
 require("dotenv").config();
 
 //  LOCAL MODULES
-const functions = require("./functions");
+const keys = require("./keys");
+
+//  API KEYS SETUP
+const Twitter = require("twitter");
+const Spotify = require("node-spotify-api");
+
+const twitter = new Twitter(keys.twitter);
+const spotify = new Spotify(keys.spotify)
 
 //  PROCESS ARGUMENTS
 const CLIObj = {
@@ -15,17 +22,79 @@ const CLIObj = {
     arg: process.argv[3],
 }
 
-switch(CLIObj.cmd) {
-    case "my-tweets":
-        functions.twitter();
-        break;
-    case "spotify-this":
-        functions.spotify(CLIObj.arg);
-        break;
-    case "movie-this":
-        functions.movie(CLIObj.arg);
-        break;
-    case "do-what-it-says":
-        functions.random();
-        break;
+
+const functions = {
+    spotify : function(search) {
+        spotify.search({ type: "track", query: search })
+            .then((resp) => {
+                let song = resp.tracks.items[0]
+                let songDeets = {
+                    name: song.name,
+                    artist: song.album.artists[0].name,
+                    album: song.album.name,
+                    preview: song.external_urls.spotify,
+                }
+                console.log(songDeets);
+            })
+            .catch((err) => {
+                throw err;
+            });
+    },
+    twitter : function() {
+        twitter.get('statuses/user_timeline', { count: 20 }, (err, tweets, resp) => {
+            if (err) throw err;
+            console.log(`Your last 20 tweets:`);
+            for (let i = 0; i < tweets.length; i++) {
+                console.log(`${tweets[i].text} | ${tweets[i]["created_at"]}`);
+            }
+        })
+    },
+    movie : function(search) {
+        request.get(`http://www.omdbapi.com/?apikey=trilogy&r=json&t=${search}`,
+            (err, res, bodyString) => {
+                if (err) throw err;
+                let body = JSON.parse(bodyString);
+                let movie = {
+                    title: body.Title,
+                    plot: body.Plot,
+                    cast: body.Actors,
+                    year: body.Year,
+                    imdb: body.Ratings[0].Value,
+                    rotten: body.Ratings[1].Value,
+                    country: body.Country,
+                    language: body.Language,
+                }
+                console.log(movie);
+            }
+        );
+    },
+    random : function() {
+        fs.readFile('random.txt', { encoding: "utf-8" }, (err, data) => {
+            data = data.split(",");
+            CmdObj = {
+                cmd: data[0],
+                arg: data[1],
+            };
+            liri(CmdObj);
+        });
+    },
 }
+
+function liri(Obj) {
+    switch(Obj.cmd) {
+        case "my-tweets":
+            functions.twitter();
+            break;
+        case "spotify-this":
+            functions.spotify(Obj.arg);
+            break;
+        case "movie-this":
+            functions.movie(Obj.arg);
+            break;
+        case "do-what-it-says":
+            functions.random();
+            break;
+    }
+} 
+
+liri(CLIObj);
